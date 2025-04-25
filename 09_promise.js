45a142611141a166a45aa1eadf4bbd7436d577b9
Aniket Singh <aniketsingh90126@gmail.com>
2025-04-25 11:17:14 +0530

    feat: promise polyfill done

---
+++ 09_promise.js
@@ -0,0 +1,67 @@
+// polyfill for promise
+
+function PromisePolyfill(executor) {
+  let onResolve,
+    onReject,
+    isFulfilled = false,
+    isRejected = false,
+    isCalled = false,
+    value;
+
+  function resolve(val) {
+    isFulfilled = true;
+    value = val;
+
+    if (typeof onResolve === "function") {
+      onResolve(val);
+      isCalled = true;
+    }
+  }
+
+  function reject(val) {
+    isRejected = true;
+    value = val;
+    if (typeof onReject === "function") {
+      onReject(val);
+      isCalled = true;
+    }
+  }
+
+  this.then = function (callback) {
+    onResolve = callback;
+
+    if (isFulfilled && !isCalled) {
+      isCalled = true;
+      onResolve(value);
+    }
+    return this;
+  };
+
+  this.catch = function (callback) {
+    onReject = callback;
+    if (isRejected && !isCalled) {
+      isCalled = true;
+      onReject(value);
+    }
+    return this;
+  };
+
+  try {
+    executor(resolve, reject);
+  } catch (error) {
+    reject(error);
+  }
+}
+
+// example for promise
+const examplePromise = new PromisePolyfill((resolve, reject) => {
+  setTimeout(() => {
+    resolve(2);
+  }, 1000);
+});
+
+examplePromise
+  .then((res) => {
+    console.log(res);
+  })
+  .catch((err) => console.log(err));
